5b89f09775b2b27d392b5bd34f792a57ff8b2765
Muhammad Jundi Al-Aqsha <muhammadjunnndi@gmail.com>
2026-09-16 08:03:25 +0700

    first commit

---
+++ soal-1.js
@@ -0,0 +1,16 @@
+let bulan = [
+  "Muharam",
+  "Shafar",
+  "Rabiul Awwal",
+  "Rabiul Akhir",
+  "Jumadal Ula",
+  "Jumadats Tsani",
+  "Rajab",
+  "Sya'ban",
+  "Ramadhan",
+  "Syawwal",
+  "Dzul qo'dah",
+  "Dzul Hijjah",
+];
+
+console.log(bulan[4]);
