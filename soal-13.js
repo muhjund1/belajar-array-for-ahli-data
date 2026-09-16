7e51a021d37c912e8a3eb08861f73ffce61a3800
Muhammad Jundi Al-Aqsha <muhammadjunnndi@gmail.com>
2026-09-16 12:32:51 +0700

    soal 13 dan soal 15

---
+++ soal-13.js
@@ -0,0 +1,5 @@
+const namaTeman = ["Fadly", "Fakhrie", "Abdullah", "Jaabir", "Jundi", "Jundi"];
+
+for (let i = 1; i <= 6; i++ ) {
+    console.log(namaTeman);
+}
---
+++ soal-14.js
@@ -0,0 +1,6 @@
+let buah = ["Apel", "Mangga", "Jeruk"];
+console.log(buah);
+
+buah.unshift("Kiwi");
+
+console.log(buah);
